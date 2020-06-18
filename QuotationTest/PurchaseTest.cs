4df6a4ffdb968af8f4d0f1c4aab9e3cff4f68c5e
yitzhak Matias <matias6@gmail.com>
2020-06-18 14:05:52 -0400

    - Purchase test was added

--- QuotationTest/PurchaseTest.cs
+++ QuotationTest/PurchaseTest.cs
@@ -1,4 +1,12 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Quotation.API.DTOs;
+using Quotation.API.Model;
 using QuotationTest.Base;
 using Xunit;
 
@@ -23,6 +31,26 @@ namespace QuotationTest
             Assert.Equal(expectedContentType,
                 response.Content.Headers.ContentType.ToString());
         }
-        
+
+        [Theory]
+       
+        [InlineData(1, 10.5,"/api/purchase/register")]
+        public async Task When_Register_Purchase_HTTP_STATUS_ERROR(int userId, double amount,string url)
+        {
+            var purchase = new  {UserId = userId, Currency = 2, Amount = amount};
+
+
+            var json = JsonConvert.SerializeObject(purchase) ;
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            using  var client = Factory.CreateClient();
+
+            var response = await client.PostAsync(url, data);
+
+            string result = response.Content.ReadAsStringAsync().Result;
+            
+            response.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+           
+        }
     }
 }
